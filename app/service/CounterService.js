a02165763a0e8c27347988f36d8e18a3ccca2f79
BrunoAgst <bruno_agst@hotmail.com>
2022-02-12 02:17:41 -0300

    created CounterService

---
+++ app/service/CounterService.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+const { GETNUMBEROFHITS, UPDATENUMBEROFHITS} = require('../util/EnumURL');
+
+class CounterService {
+    constructor(){}
+
+    async getNumberOfHits(){
+        const url = GETNUMBEROFHITS;
+        const { data } = await axios.get(url);
+        return data;
+    }
+
+    async updateNumberOfHits(){
+        const url = UPDATENUMBEROFHITS;
+        return await axios.get(url);
+    }
+}
+
+module.exports = new CounterService();
