c0c50455c75a86197e819e51d63fef8ee9e8f8ce
BrunoAgst <bruno_agst@hotmail.com>
2022-02-13 15:54:18 -0300

    configured express.json and inicialized database

--- app/handler.js
+++ app/handler.js
@@ -1,11 +1,15 @@
 'use-strict'
 
+require('./config/database')()
+
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
 const CounterController = require('./controller/CounterController');
 const UserController = require('./controller/UserController');
+
+app.use(express.json());
 
 app.get("/counter", CounterController.get);
 app.put("/counter", CounterController.update);
