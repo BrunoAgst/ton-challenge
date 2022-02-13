6806fbd92d02ed989f2821adfc5391f0a9074f5b
BrunoAgst <bruno_agst@hotmail.com>
2022-02-13 15:53:22 -0300

    installed mongoose and configured database

---
+++ app/config/database.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const { connect, connection } = require('mongoose');
+
+module.exports = () => {
+    const url = 'mongodb://localhost:27017/ton';
+    connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    connection.once('open', () => { console.log('database connected') });
+}
