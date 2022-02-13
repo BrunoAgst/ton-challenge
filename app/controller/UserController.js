d76ebc461f2a77e9a1eee394c9c1d85f17330e37
BrunoAgst <bruno_agst@hotmail.com>
2022-02-13 15:57:00 -0300

    update function create in UserController

--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -1,5 +1,7 @@
 'use-strict'
 
+const { createUser } = require('../service/UserService');
+
 class UserController {
     constructor() {}
 
@@ -10,9 +12,25 @@ class UserController {
     }
 
     async create(request, response){
-        return response.status(200).json({
-            message: 'ok'
-        })
+
+        try {
+            
+            const payload = request.body;
+            
+            const { code, message } = await createUser(payload);
+
+            if(code === 400) {
+                return response.status(code).json({ message: message });
+            }
+
+            return response.status(200).json({
+                message: 'created success'
+            })
+            
+        } catch (error) {
+            console.log(error);
+            return response.status(503).json({ message: "Service Unavailable" });
+        }
     }
 }
 
