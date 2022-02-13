c4ddf6f43a5cf4b203235eeae633fdfa847268c5
BrunoAgst <bruno_agst@hotmail.com>
2022-02-13 19:18:42 -0300

    update User schema

--- app/schema/User.js
+++ app/schema/User.js
@@ -2,11 +2,25 @@
 
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-    name: String,
-    age: Number,
-    email: String,
-    password: String
-});
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number
+        },
+        email: { 
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    { timestamps: true }
+);
 
 module.exports = model('User', userSchema);
