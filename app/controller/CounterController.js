1accf214b13ad7f36cede4ecce8484d7991e70d8
BrunoAgst <bruno_agst@hotmail.com>
2022-02-14 03:03:23 -0300

    changed CounterController

--- app/controller/CounterController.js
+++ app/controller/CounterController.js
@@ -1,26 +1,34 @@
 'use-strict'
 
-const { getNumberOfHits, updateNumberOfHits } = require("../service/CounterService");
+const CounterService = require("../service/CounterService");
 class CounterController {
     constructor(){}
 
     async get(request, response) {
         try {
-            const { value } = await getNumberOfHits();
-            return response.status(200).json({ "Number of hits": value });
+            
+            const { value } = await CounterService.getNumberOfHits();
+            response.status(200);
+            response.json({ "Number of hits": value });
+
         } catch (error) {
             console.log(error);
-            return response.status(503).json({ message: "Service Unavailable" });
+            response.status(503);
+            response.json({ message: "Service Unavailable" });
         }
     }
 
     async update(request, response) {
         try {
-            await updateNumberOfHits();
-            return response.status(200).json({ message: "Update success" });
+
+            await CounterService.updateNumberOfHits();
+            response.status(200);
+            response.json({ message: "Update success" });
+
         } catch (error) {
             console.log(error);
-            return response.status(503).json({ message: "Service Unavailable" });
+            response.status(503);
+            response.json({ message: "Service Unavailable" });
         }
     }
 }
