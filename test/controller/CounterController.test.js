a8a825f845de67e85ea673aba0501cb4ce6b8452
BrunoAgst <bruno_agst@hotmail.com>
2022-02-14 09:41:01 -0300

    update CounterController test

--- test/controller/CounterController.test.js
+++ test/controller/CounterController.test.js
@@ -1,12 +1,94 @@
 'use strict'
 
 const { describe, test, expect, jest: _jest, beforeEach } = require("@jest/globals");
-const CounterController = require('../../app/controller/CounterController');
-const CounterService = require('../../app/service/CounterService');
+const CounterController = require("../../app/controller/CounterController");
+const CounterService = require("../../app/service/CounterService");
 
 describe('#CounterController', () => {
 
     beforeEach(() => _jest.clearAllMocks());
     
-    test.todo('should return status 200 and number of hits')
+    test('should return status 200 and number of hits', async () => {
+
+        _jest.spyOn(
+            CounterService,
+            'getNumberOfHits'
+        ).mockResolvedValue({
+            value: 200
+        });
+
+        const request = {};
+
+        const response = {
+            status: () => 200,
+            json: _jest.fn()
+        };
+
+        const result = await CounterController.get(request, response);
+        
+        expect(result).toBeUndefined();
+    })
+
+    test('should return error', async () => {
+        _jest.spyOn(
+            CounterService,
+            'getNumberOfHits'
+        ).mockImplementationOnce(() => Promise.reject('Service Unavailable'));
+        
+        const request = {
+            body: _jest.fn()
+        };
+
+        const response = {
+            status: () => 503,
+            json: _jest.fn()
+        };
+        
+        const result = await CounterController.get(request, response);
+
+        expect(result).toBeUndefined();
+
+    })
+
+    test('should return status 200 and number of hits', async () => {
+
+        _jest.spyOn(
+            CounterService,
+            'updateNumberOfHits'
+        ).mockResolvedValue({
+            value: 200
+        });
+
+        const request = {};
+
+        const response = {
+            status: () => 200,
+            json: _jest.fn()
+        };
+
+        const result = await CounterController.update(request, response);
+        
+        expect(result).toBeUndefined();
+    })
+
+    test('should return error', async () => {
+        _jest.spyOn(
+            CounterService,
+            'updateNumberOfHits'
+        ).mockImplementationOnce(() => Promise.reject('Service Unavailable'));
+        
+        const request = {
+            body: _jest.fn()
+        };
+
+        const response = {
+            status: () => 503,
+            json: _jest.fn()
+        };
+        
+        const result = await CounterController.update(request, response);
+
+        expect(result).toBeUndefined();
+
+    })
 })
